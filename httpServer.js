ad482a05702016f847e6c4ee73df2f277d4e4070
Lauren Risberg <lauren@Laurens-MacBook-Pro.local>
2017-03-14 20:19:01 -0600

    add server

---
+++ httpServer.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+
+var http = require('http');
+var port = process.env.PORT || 8000;
+
+var server = http.createServer(function(req, res) {
+  if (req.method === 'GET' && req.url === '/pets') {
+    fs.readFile('pets.json', 'utf8', function(err, petsJSON) {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(petsJSON);
+    });
+  }
+  else if (req.method === 'GET' && req.url === '/pets/0') {
+    fs.readFile('pets.json', 'utf8', function(err, petsJSON) {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
+
+      var pets = JSON.parse(petsJSON);
+      var petJSON = JSON.stringify(pets[0]);
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(petJSON);
+    });
+  }
+  else if (req.method === 'GET' && req.url === '/pets/1') {
+    fs.readFile('pets.json', 'utf8', function(err, petsJSON) {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
+
+      var pets = JSON.parse(petsJSON);
+      var petJSON = JSON.stringify(pets[1]);
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(petJSON);
+    });
+  }
+  else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not found');
+  }
+});
+
+server.listen(port, function() {
+  console.log('Listening on port', port);
+});
--- pets.js
+++ pets.js
@@ -1,8 +1,3 @@
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
-
 const node = path.basename(process.argv[0]);
 const file = path.basename(process.argv[1]);
 const cmd = process.argv[2];
@@ -112,8 +107,6 @@ else if (cmd === 'destroy') {
     });
   });
 }
-
-
 
 else {
   console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
