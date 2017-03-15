cdd76e0ce0790dc6858296825f5d1dec20368834
Lauren Risberg <lauren@Laurens-MacBook-Pro.local>
2017-03-15 17:27:02 -0600

    completed express part

--- pets.js
+++ pets.js
@@ -1,9 +1,13 @@
-const fs = require('fs');
-const path = require('path');
-const petsPath = path.join(__dirname, 'pets.JSON');
+let fs = require('fs');
+let path = require('path');
+let petsPath = path.join(__dirname, 'pets.JSON');
 let args = process.argv;
 let subCommand = args[2];
 
+if (!args[2]) {
+    console.error('Usage: node pets.js [read | create | update | destroy]');
+    process.exit(1);
+}
 
 fs.readFile(petsPath, 'utf8', (err, petsData) => {
   let parsedPets = JSON.parse(petsData);
@@ -13,16 +17,7 @@ fs.readFile(petsPath, 'utf8', (err, petsData) => {
     }
 
     else if (args[3] >= 0 && args[3] < parsedPets.length) {
-      console.log('IN');
+      console.log(parsedPets[args[3]]);
     }
   }
 })
-
-
-
-
-
-// if (err) {
-//   console.error('Usage: node pets.js [read | create | update | destroy]');
-//   process.exit(1);
-// }
